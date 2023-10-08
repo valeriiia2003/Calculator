9e481191e2dc660a41f39bf77f024603f914b98d
leraspasibuhova <V.Spasibukhova.FIT.121.20@knute.edu.ua>
2023-10-08 23:48:54 +0100

    Showing how the add method works at class program

--- userCalculator/userCalculator/Program.cs
+++ userCalculator/userCalculator/Program.cs
@@ -4,7 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        try
+        {
+            Console.WriteLine("Enter two nums to use userCalculaor: ");
+
+            int firstArg = Convert.ToInt32(Console.ReadLine());
+            int secondArg = Convert.ToInt32(Console.ReadLine());
+            int result = new Operations().AddNums(firstArg, secondArg);
+
+            Console.WriteLine($"Result of adding nums = {result}");
+        }
+        catch 
+        {
+            throw new FormatException();
+        }finally { Console.WriteLine("\nWe have been working at userCalculator"); }
+        Console.ReadKey();
     }
 }
 
